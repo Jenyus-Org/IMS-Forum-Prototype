782c3fee0502df26b54d83969b3a9833a6c2ad72
Dan6erbond <moravrav@gmail.com>
2020-09-26 15:11:46 +0200

    Create `Submission` and `Tutorial` classes
    Inheriting from `Post` containing meta info as well as `Title` and `Body`.

---
+++ Models/Tutorial.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace IMSForumPrototype.Models
+{
+    public class Tutorial : Post
+    {
+        public int Id { get; }
+        public DateTime CreatedAt { get; }
+        public User CreatedBy { get; }
+        public string Title { get; }
+        public string Body { get; set; }
+        public User Author { get; }
+        public bool Removed { get => this.RemovedBy != null; }
+        public User RemovedBy { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public bool Deleted { get => this.DeletedAt != null; }
+        public CommentForest Comments { get; set; }
+
+        public Tutorial(int id, DateTime createdAt, User createdBy, string body, User author)
+        {
+            this.Id = id;
+            this.CreatedAt = createdAt;
+            this.CreatedBy = createdBy;
+            this.Body = body;
+            this.Author = author;
+        }
+    }
+}
