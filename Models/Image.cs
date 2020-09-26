02317c624eb903cd241a03eb5cf1113d066401f0
Dan6erbond <moravrav@gmail.com>
2020-09-26 15:12:10 +0200

    Create `Image` model
    Containing additional meta information from `Thing`.

---
+++ Models/Image.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace IMSForumPrototype.Models
+{
+    public class Image : Thing
+    {
+        public int Id { get; }
+        public DateTime CreatedAt { get; }
+        public User CreatedBy { get; }
+        public bool Removed { get => this.RemovedBy != null; }
+        public User RemovedBy { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public bool Deleted { get => this.DeletedAt != null; }
+
+        public Image(int id, DateTime createdAt, User createdBy)
+        {
+            this.Id = id;
+            this.CreatedAt = createdAt;
+            this.CreatedBy = createdBy;
+        }
+    }
+}
