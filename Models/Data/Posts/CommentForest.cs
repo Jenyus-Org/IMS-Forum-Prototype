62220dd70db44a1547c598e007c7c73686e05eb4
Dan6erbond <moravrav@gmail.com>
2020-10-01 23:14:06 +0200

    Add O2O relationship between comment forests and posts

--- Models/Data/Posts/CommentForest.cs
+++ Models/Data/Posts/CommentForest.cs
@@ -8,6 +8,10 @@ namespace IMSForumPrototype.Models.Data.Posts
     public class CommentForest : Thing
     {
         public int Id { get; }
+        public int SubmissionId { get; set; }
+        public int TutorialId { get; set; }
+        public int SubmissionId { get; set; }
+        public int TutorialId { get; set; }
         public DateTime CreatedAt { get; }
         public User CreatedBy { get; }
         public DateTime DeletedAt { get; set; }
@@ -15,9 +19,11 @@ namespace IMSForumPrototype.Models.Data.Posts
         public bool Deleted { get => this.DeletedAt != null && this.DeletedBy != null; }
         public List<Comment> Comments { get; } = new List<Comment>();
 
-        public CommentForest(int id, DateTime createdAt, User createdBy)
+        public CommentForest(int id, int submissionId, int tutorialId, DateTime createdAt, User createdBy)
         {
             this.Id = id;
+            this.SubmissionId = submissionId;
+            this.TutorialId = tutorialId;
             this.CreatedAt = createdAt;
             this.CreatedBy = createdBy;
         }
