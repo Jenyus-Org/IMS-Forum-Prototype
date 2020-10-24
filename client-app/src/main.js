677a8e49b0bb46cb2f51bf87cae3f70dc16cf732
Dan6erbond <moravrav@gmail.com>
2020-10-24 11:23:56 +0200

    Setup API SDK in client app

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -1,26 +1,34 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import './plugins/axios'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import "./plugins/axios";
+import "./plugins/strapi";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserSecret, faCogs, faHome, faChalkboard, faSearch, faHeart} from "@fortawesome/free-solid-svg-icons";
+import {
+  faUserSecret,
+  faCogs,
+  faHome,
+  faChalkboard,
+  faSearch,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faUserSecret)
-library.add(faCogs)
-library.add(faHome)
-library.add(faChalkboard)
-library.add(faSearch)
-library.add(faHeart)
+library.add(faUserSecret);
+library.add(faCogs);
+library.add(faHome);
+library.add(faChalkboard);
+library.add(faSearch);
+library.add(faHeart);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
